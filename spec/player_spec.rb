38fcc1f028a34320bee83426748756d5d7c7d809
Chris Lawrence <chris.lawrence@gmx.com>
2016-10-12 15:23:21 +0100

    Added Player class, created global variable for each player, held in the name attribute, tests passing

---
+++ spec/player_spec.rb
@@ -0,0 +1,14 @@
+require 'player'
+
+describe Player do
+
+  subject(:player) { described_class.new("name") }
+
+  describe "#name" do
+    it "returs players name" do
+      expect(player.name).to eq "name"
+    end
+  end
+
+
+end
