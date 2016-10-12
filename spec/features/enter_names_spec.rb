c6ae4bc2f506620d38f0696c275f89998fa716d1
Chris Lawrence <chris.lawrence@gmx.com>
2016-10-12 12:30:35 +0100

    Added web_helper doc, and created #sign_in_and_play. refactored tests, passing.

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: "Kanye"
-    fill_in :player_2_name, with: "Kittens"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Kanye Vs. Kittens'
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 feature 'See player 2\'s hit points' do
   scenario 'shows the the health of player 2' do
-    visit('/')
-    fill_in :player_1_name, with: "Kanye"
-    fill_in :player_2_name, with: "Kittens"
-    click_button 'Submit'
-
-    expect(page.text).to include("Player 2's Hit Points : 100HP")
+    sign_in_and_play
+    expect(page.text).to include("Kittens's Hit Points : 100HP")
   end
 end
