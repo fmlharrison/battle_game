e307d4991621ae59b5126e9aea9c39a07a2f2e7b
Malinna Leach <malinnab@yahoo.com>
2016-10-14 12:38:28 +0100

    adding restart button

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -9,4 +9,11 @@ feature 'game over' do
     expect(page).to have_content "Kittens has been defeated and has brought shame to their family"
   end
 
+  scenario 'game can be restarted' do
+    sign_in_and_play
+    18.times {single_turn}
+    click_button('Attack!')
+    click_button('Play Again')
+    expect(page).to have_content "Battle Royale!"
+  end
 end
